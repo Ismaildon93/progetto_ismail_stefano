9e98d8106be3ea9d37920f1677629e4c671e1d9c
Stefano <ste.valladares@live.com>
2017-11-01 19:53:34 +0100

    Add all

---
+++ src/main/java/Passeggero.java
@@ -0,0 +1,19 @@
+	
+public class Passeggero {
+	
+	private String nome;
+	private String cognome;
+	
+	public Passeggero(String nome, String cognome) {
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public String getCognome() {
+		return this.cognome;
+	}
+}
