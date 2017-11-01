6f80bbaccf5dc120a3b5a917e47b7cc0179e394a
ismail.dona <ismaildon93@gmail.com>
2017-11-01 21:20:27 +0100

    asdg

--- src/main/java/vehicle/Pulman.java
+++ src/main/java/vehicle/Pulman.java
@@ -1,10 +1,14 @@
 package vehicle;
+import java.util.ArrayList;
+import java.util.List;
 
+import PORCODIO.*;
 
 public class Pulman extends Veicolo {
 
 	private String societa;
 	private int numPosti;
+	private List<Passeggero> passeggeri;
 	
 	public Pulman(String societa, int numPosti, String targa, String numTelaio, 
 						int cilindrata, int pistoni){
@@ -12,7 +16,15 @@ public class Pulman extends Veicolo {
 		super(targa, numTelaio, cilindrata, pistoni);
 		this.societa = societa;
 		this.numPosti = numPosti;
+		this.passeggeri = new ArrayList<Passeggero>();
 		
+	}
+	
+	public int addPasseggero(Passeggero passeggero){
+		
+		passeggeri.add(passeggero);
+		
+		return passeggeri.size();
 	}
 	
 	public String getSocieta(){
