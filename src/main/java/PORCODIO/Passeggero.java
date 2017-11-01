37b70d5b4fb495513fe9d6a05741967b5c8b08e2
Stefano <ste.valladares@live.com>
2017-11-01 21:16:37 +0100

    guhbnjk

--- src/main/java/PORCODIO/Passeggero.java
+++ src/main/java/PORCODIO/Passeggero.java
@@ -1,14 +1,28 @@
 package PORCODIO;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import vehicle.Pulman;
 
 public class Passeggero {
 	
 	private String nome;
 	private String cognome;
+	private List<Pulman> pulmans;
 	
 	public Passeggero(String nome, String cognome) {
 		this.nome = nome;
 		this.cognome = cognome;
+		this.pulmans = new ArrayList<Pulman>();
+	}
+	
+	public void addPulman(Pulman pulman) {
+		pulmans.add(pulman);
+	}
+	
+	public List<Pulman> getPulmans() {
+		return pulmans;
 	}
 	
 	public String getNome() {
